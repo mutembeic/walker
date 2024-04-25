3882f3cfeb8b87e265cd1935aaf779afedea5f2c
mutembeic <135004069+mutembeic@users.noreply.github.com>
2024-04-25 12:08:03 +0300

    added index.js

---
+++ script/index.js
@@ -0,0 +1,48 @@
+const langSkills = document.querySelectorAll(".languages ul li span");
+let started = false;
+const codeSkills = document.querySelectorAll(".wrapper .exp li span");
+let startedTwo = false;
+
+
+window.onscroll = () => {
+    if (window.scrollY > 50) {
+        if (!startedTwo) {
+            let startIndex = 0;
+            codeSkills.forEach((el) => {
+                let interValid = setInterval(() => {
+                    el.computedStyleMap.width =  `${startIndex++}%`;
+                    if (parseInt(startIndex) >= parseInt(el.dataset.width)) {
+                        clearInterval(interValid);
+                    }
+                }, 4000 / parseInt(el.dataset.width));
+            });
+        };
+        startedTwo = true;
+    }
+    if (window.scrollY > 130) {
+        if (!started) {
+            let startIndex = 0;
+            langSkills.forEach((span) => {
+                let interValid = setInterval(() => {
+                    span.computedStyleMap.width =  `${startIndex++}%`;
+                    if (parseInt(startIndex) >= parseInt(span.dataset.width)) {
+                        clearInterval(interValid);
+                    }
+                }, 4000 / parseInt(span.dataset.width));
+            });
+        };
+        started = true;
+    }
+}
+
+
+//More Skills
+const root = document.documentElement;
+const marqueenElementDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
